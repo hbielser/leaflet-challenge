2074a52b4257e7cfd955a8afc440f69ff5251537
hali b <hali.bielser@gmail.com>
2020-11-11 21:48:28 -0600

    Psudo Code

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiaGFsaWIiLCJhIjoiY2toN3U3YThnMDNoNTJ4cHA3eTU0azFjbyJ9.lKxrGhA2N_hM5lTnGHiX3Q";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,6 @@
+// Create a map using Leaflet that plots all the earthquakes from you data set based on long/lat
+// Your data markers should reflect the magnitude of the earthquake by their size and and depth of the earth quake by color. Earthquakes with higher 
+// magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
+// HINT the depth of the earth can be found as the third coordinate for each earthquake.
+// Include popups that provide additional information about the earthquake when a marker is clicked.
+// Create a legend that will provide context for your map data.
