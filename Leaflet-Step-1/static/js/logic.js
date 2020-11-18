dbd5e1aaea1b37b15fdf5d70b770aec6b33401f5
hali b <hali.bielser@gmail.com>
2020-11-18 12:52:05 -0600

    Progress as of 11/18/20

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -20,10 +20,12 @@ var myMap = L.map('map').setView([37.8, -96], 4);
   }).addTo(myMap);
 
 // Load in geojson data
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+var geojson;
 
 // Grab the data with d3
-d3.json(url, function(response) {
+d3.json(geoData, function(response) {
 
     // // confirm response
     // console.log(response)
@@ -34,10 +36,10 @@ d3.json(url, function(response) {
     // Loop through data
     for (var i = 0; i < features.length; i++) {
       var lat = features[i]["geometry"]["coordinates"][1];
-      var long = features[i]["geometry"]["coordinates"][0];
+      var lng = features[i]["geometry"]["coordinates"][0];
       var depth = features[i]["geometry"]["coordinates"][2];
       var mag = features[i]["properties"]["mag"];
-      var title = features[i]["properties"]["title"]
+      var title = features[i]["properties"]["title"];
 
     //   console.log(mag)
 
@@ -62,41 +64,45 @@ d3.json(url, function(response) {
             color = "Green";
         }
 
-      L.circle([lat, long], {
+      L.circle([lat, lng], {
           color: color,
           fillOpacity: 0.75,
           radius: mag * 5000,
       }).bindPopup("<h1> Description: " + title + "</h1>" + "<hr>" + "<h2> Depth: " + depth + "<h2/>")
         .addTo(myMap);
 
-        // Set up the legend - Module 17.2, activity 4
-        var legend = L.control({ position: "bottomright" });
-        legend.onAdd = function() {
-            var div = L.DomUtil.create("div", "info legend");
-            var limits = geojson.options.limits;
-            var colors = geojson.options.colors;
-            var labels = [];
-
-            // Add min & max
-            var legendInfo = "<h1>Earthquake Depth Scale</h1>" +
-            "<div class=\"labels\">" +
-                "<div class=\"min\">" + limits[0] + "</div>" +
-                "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-            "</div>";
-
-            div.innerHTML = legendInfo;
-
-            limits.forEach(function(limit, index) {
-            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-            });
-
-            div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-            return div;
-        };
-
-        // Adding legend to the map
-        legend.addTo(myMap);
-
     };
+
+    colorList = ["Red","OrangeRed","Orange","Yellow","GreenYellow","Green"]
+    depthList = ["90", "70","50","30","10","<10"]
+        
+    // Set up the legend - Module 17.2, activity 4
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        // var limits = depth.limits;
+        var labels = [];
+
+        // Add min & max
+        var legendInfo = "<h1>Earthquake Depth Scale</h1>" 
+        // "<div class=\"labels\">" +
+        //     "<div class=\"min\">" + "-10" + "</div>" +
+        //     "<div class=\"max\">" + "90" + "</div>" +
+        // "</div>";
+
+        div.innerHTML = legendInfo;
+
+        colorList.forEach(function(color, index) {
+        labels.push("<li style=\"background-color: " + color + "\">" + 
+            "<i>" + (depthList[index]) + "</i>" + 
+          "</li>");
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    // Adding legend to the map
+    legend.addTo(myMap);
 
   });
